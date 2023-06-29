972972fc993e9d79c428ab20a681f6e24f2c8965
Anush-Krish <107431883+Anush-Krish@users.noreply.github.com>
2023-06-29 15:38:10 +0530

    database setup

--- kafka-consumer-database/src/main/java/net/javaguides/springboot/KafkaDatabaseConsumer.java
+++ kafka-consumer-database/src/main/java/net/javaguides/springboot/KafkaDatabaseConsumer.java
@@ -1,6 +1,8 @@
 package net.javaguides.springboot;
 
 
+import net.javaguides.springboot.entity.WikimediaData;
+import net.javaguides.springboot.repository.WikimediaDataRepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -10,10 +12,23 @@ import org.springframework.stereotype.Service;
 public class KafkaDatabaseConsumer {
     private static final Logger LOGGER= LoggerFactory.getLogger(KafkaDatabaseConsumer.class);
 
+    public KafkaDatabaseConsumer(WikimediaDataRepo dataRepo) {
+        this.dataRepo = dataRepo;
+    }
+
+    private WikimediaDataRepo dataRepo;
+
+
     @KafkaListener(topics = "wikimedia_recentchanges",
                    groupId= "myGroup")
     public void consumer(String eventMessage){
         LOGGER.info(String.format("Event message received ->%s",eventMessage));
+
+
+        WikimediaData wikimediaData=new WikimediaData();
+        wikimediaData.setWikiEventData(eventMessage);
+
+        dataRepo.save(wikimediaData);
 
     }
 }
